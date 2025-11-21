b7e4c30828605c27a4d6941644152ce5bce7a71d
PilarBasaldua <124049109@upq.edu.mx>
2025-11-21 15:27:00 -0600

    Boton de iniciar sesion

--- screens/GraphScreen.js
+++ screens/GraphScreen.js
@@ -5,6 +5,8 @@ import CrudModal from "./CrudModal.js";
 import AppHeader from "../components/AppHeader";
 
 export default function graphScreen() {
+    const [showModal, setShowModal] = useState(false);
+    const toggleModal = () => setShowModal(!showModal);
 
     return (
         <View style={styles.page}>
@@ -92,6 +94,8 @@ export default function graphScreen() {
                     </View>
                 </View>
             </ScrollView>
+            <Navbar toggleModal={toggleModal}/>
+            <CrudModal visible={showModal} setVisible={setShowModal}/>
         </View>
     );
 }
