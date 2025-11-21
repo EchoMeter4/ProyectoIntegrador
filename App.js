a030f8a37f17bdc12db23cd52d8d8228eda1d69b
AbdielGP-214 <124050294@upq.edu.mx>
2025-11-21 15:17:44 -0600

    login ya manda a la principal

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {StyleSheet} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GraphScreen from './screens/GraphScreen';
+import LoginScreen from './screens/LoginScreen';
 import ListScreen from './screens/TransactionScreen'
 import {NavigationContainer} from "@react-navigation/native";
 import Navbar from "./components/Navbar";
@@ -8,17 +9,32 @@ import CrudModal from "./screens/CrudModal";
 import React, {useState} from "react";
 
 const MainStack = createNativeStackNavigator();
+
 export default function App() {
     const [showModal, setShowModal] = useState(false);
+    
+    const [currentRoute, setCurrentRoute] = useState('Login'); 
+
     const toggleModal = () => setShowModal(!showModal);
 
     return (
-        <NavigationContainer>
+        <NavigationContainer
+            
+            onStateChange={(state) => {
+                
+                const routeName = state?.routes[state.index]?.name;
+                setCurrentRoute(routeName);
+            }}
+        >
             <MainStack.Navigator
                 id='mainStack'
-                initialRouteName='Graph'
+                initialRouteName='Login'
                 screenOptions={{headerShown: false}}
             >
+                <MainStack.Screen
+                    name='Login'
+                    component={LoginScreen}
+                />
                 <MainStack.Screen
                     name='Graph'
                     component={GraphScreen}
@@ -28,9 +44,15 @@ export default function App() {
                     component={ListScreen}
                 />
             </MainStack.Navigator>
-            <Navbar toggleModal={toggleModal}/>
-            <CrudModal visible={showModal} setVisible={setShowModal}/>
+
+           
+            {currentRoute !== 'Login' && (
+                <>
+                    <Navbar toggleModal={toggleModal}/>
+                    <CrudModal visible={showModal} setVisible={setShowModal}/>
+                </>
+            )}
+            
         </NavigationContainer>
     )
 }
-
